2163927fa3b5bbf8712a00ed9fcf91d3003ccdbb
Joe Heath <joe.heath@gmail.com>
2012-07-26 16:21:40 +0100

    parameterised xor test for activation functions

--- test/EncogExamples/XORNetwork.java
+++ test/EncogExamples/XORNetwork.java
@@ -15,13 +15,15 @@ import org.junit.Test;
 
 public class XORNetwork {
 
-	private String network_script = "network basic_net is basic {\n"
+	private String getNetworkScript(String activation) {
+		return  "network basic_net is basic {\n"
 			+ "    layer {\n" + "        activation input\n"
 			+ "        size 2\n" + "        biased\n" + "    }\n	 "
-			+ "    layer {\n" + "        activation sigmoid\n"
+			+ "    layer {\n" + "        activation "+activation+"\n"
 			+ "        size 3\n" + "        biased \n" + "    }\n"
 			+ "    layer {\n" + "        activation sigmoid\n"
 			+ "        size 1\n" + "    }\n" + "}\n";
+	}
 
 	private String training_script = "training {\n"
 			+ "  type resilient_propagation\n" + "  error 0.1%\n"
@@ -33,21 +35,27 @@ public class XORNetwork {
 	@Test
 	public void createNetwork() {
 		ScriptParser parser = new ScriptParser();
-		Network network = parser.parseScript(network_script + training_script);
+		Network network = parser.parseScript(getNetworkScript("sigmoid") + training_script);
 		assertNotNull(network);
 		assertEquals("basic", network.getType());
 		assertEquals("basic_net", network.getName());
 	}
 
 	@Test
-	public void trainNetwork() {
+	public void testActivation() {
+		trainNetwork("sigmoid");
+		//trainNetwork("tanh");
+	}
+	
+	
+	private void trainNetwork(String activation) {
 
 		double[][] input = new double[][] { { 0.0, 0.0 }, { 1.0, 0.0 },
 				{ 0.0, 1.0 }, { 1.0, 1.0 } };
 		double[][] output = new double[][] { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
 
 		ScriptParser parser = new ScriptParser();
-		Network network = parser.parseScript(network_script + training_script);
+		Network network = parser.parseScript(getNetworkScript(activation) + training_script);
 		network.train();
 		double[] result = new double[1];
 		for (int i = 0; i < 4; i++) {
